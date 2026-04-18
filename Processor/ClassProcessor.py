a72017ae92674256acfa267f9e95fcc75b60720e
Anderson Oliveira proScore <anderson@proscore.com.br>
2026-04-18 09:13:48 -0300

    correcoes

--- Processor/ClassProcessor.py
+++ Processor/ClassProcessor.py
@@ -180,13 +180,13 @@ class Processor:
 
 
     def executar_ciclo(self):
-        # self.executar()   
+        self.executar()   
         # self.enviar_email()
         # self.busca_dados()   
         # self.teste_busca_interpol()   
         self.from_name_interpol()   
         # self.atualiza_dados_interpol()
-        # self.match_name()
+        self.match_name()
         ClassLogger.logger.info(f"[{time.strftime('%H:%M:%S')}] Iniciando a consulta")      
     def executar_ciclo_name(self):
         ClassLogger.logger.info(f"[{time.strftime('%H:%M:%S')}] Iniciando a consulta From Name Primeira letras")     
