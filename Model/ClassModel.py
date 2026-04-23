2150d88eab3b1a6139bf82ee129af4eb1781c5a1
Anderson Oliveira proScore <anderson@proscore.com.br>
2026-04-23 08:35:25 -0300

    ajuste

--- Model/ClassModel.py
+++ Model/ClassModel.py
@@ -525,7 +525,7 @@ def get_lista_name_base_interpol(self) -> List[Dict]:
 #PROCESSO INVERSO PEGANDO OS IDS 
 def list_interpol(self) -> List[Dict]:
       query = """SELECT id_interpol AS ID_INTERPOL FROM public.interpol_dados 
-                 WHERE id_interpol IS NOT NULL AND situacao = true ORDER BY id_interpol desc limit 10"""
+                 WHERE id_interpol IS NOT NULL AND situacao = true ORDER BY id_interpol desc"""
       
       
       try:
