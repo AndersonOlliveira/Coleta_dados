3727c8a0609a69ef423ad20fb4989b6f1933c740
Anderson Oliveira proScore <anderson@proscore.com.br>
2026-04-24 08:47:03 -0300

    DESCOMENTADO FUNCOES PARA A CHAMADA

--- Processor/ClassProcessor.py
+++ Processor/ClassProcessor.py
@@ -200,13 +200,13 @@ class Processor:
 
 
     def executar_ciclo(self):
-        # self.executar()   
+        self.executar()   
         # self.enviar_email()
         # self.busca_dados()   
         # self.teste_busca_interpol()   
         self.from_name_interpol()   
-        # self.match_name()
-        # self.atualiza_dados_interpol()
+        self.match_name()
+        self.atualiza_dados_interpol()
         # self.lista_dados_maior_que_limite()
         ClassLogger.logger.info(f"[{time.strftime('%H:%M:%S')}] Iniciando a consulta")      
     def executar_ciclo_name(self):
